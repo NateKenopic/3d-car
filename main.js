a7b371ee2428a4dc856f3438644fcffb69b89af8
Nate-K0 <nkenopic@myseneca.ca>
2023-04-27 14:10:39 -0400

    updated website

---
+++ main.js
@@ -0,0 +1,70 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xdddddd);
+
+const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 5000);
+camera.rotation.y = 45 / 180 * Math.PI;
+camera.position.x = 800;
+camera.position.y = 100;
+camera.position.z = 1000;
+
+scene.add(camera);
+
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({canvas});
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 10;
+
+
+const hlight = new THREE.AmbientLight(0x404040, 100);
+scene.add(hlight);
+
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 100);
+directionalLight.position.set(0, 1, 0);
+directionalLight.castShadow = true;
+
+scene.add(directionalLight);
+
+
+const light = new THREE.PointLight(0xc4c4c4, 10);
+light.position.set(0, 300, 500);
+scene.add(light);
+
+const light2 = new THREE.PointLight(0xc4c4c4, 10);
+light2.position.set(500, 100, 0);
+scene.add(light2);
+
+const light3 = new THREE.PointLight(0xc4c4c4, 10);
+light3.position.set(0, 100, -500);
+scene.add(light3);
+
+const light4 = new THREE.PointLight(0xc4c4c4, 10);
+light4.position.set(-500, 300, 500);
+scene.add(light4);
+
+
+const loader = new GLTFLoader();
+loader.load('./scene.gltf', function (gltf) {
+    let car = gltf.scene.children[0];
+    car.scale.set(0.5, 0.5, 0.5);
+    scene.add(gltf.scene);
+    // animate();
+    renderer.render(scene, camera);
+});
+
+function animate() {
+    controls.update();
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(animate);
+}
